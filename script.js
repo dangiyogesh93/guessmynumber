994182036cbd8667697e2565bc252dbc1ffa1e62
dangiyogesh93 <dangi.yogesh93@gmail.com>
2021-01-24 12:28:04 +0530

    js code updated

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 'use strict';
 // document.querySelector('#scoreObtained').textContent = 50;
 
-const secretNumber = Math.floor(Math.random() * 20 + 1);
+let secretNumber = Math.floor(Math.random() * 20 + 1);
 let score = 20;
 let highScore = 0;
 
 document.querySelector('.btn').addEventListener('click', function () {
   const userNumber = Number(document.querySelector('#guess').value);
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('#message').style.color = '#eee';
 
   if (!userNumber) {
     document.querySelector('#message').textContent = 'No Number!!! inserted';
@@ -19,9 +21,12 @@ document.querySelector('.btn').addEventListener('click', function () {
     document.querySelector('#secretNumber').textContent = secretNumber;
     document.querySelector('#scoreObtained').style.color = 'green';
     score++;
-    highScore = score;
     document.querySelector('#scoreObtained').textContent = score;
-    document.querySelector('#highscore').textContent = highScore;
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('#highscore').textContent = highScore;
+    }
   } else {
     if (score > 1) {
       if (userNumber > secretNumber) {
@@ -37,4 +42,16 @@ document.querySelector('.btn').addEventListener('click', function () {
         'You Lose the Game!!! Play  Again';
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('#message').textContent = 'Start Guessing...';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('#message').style.color = '#eee';
+  document.querySelector('#guess').value = '';
+  document.querySelector('#secretNumber').textContent = '?';
+  secretNumber = Number(Math.floor(Math.random() * 20 + 1));
+  score = 20;
+  document.querySelector('#scoreObtained').textContent = score;
+  secretNumber = Number(Math.floor(Math.random() * 20 + 1));
 });
